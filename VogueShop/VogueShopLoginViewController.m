60040062fb527bb46da6c729d7641b6120233ade
wanming zhang <wzhang2@apple.com>
2016-09-21 16:19:47 -0700

    Setup login button.

--- VogueShop/VogueShopLoginViewController.m
+++ VogueShop/VogueShopLoginViewController.m
@@ -7,11 +7,12 @@
 //
 
 #import "VogueShopLoginViewController.h"
-
+#define LOGIN_BUTTON_TITLE @"Login"
 
 @interface VogueShopLoginViewController ()
 @property (weak, nonatomic) IBOutlet UIImageView *backgroundImage;
 @property (weak, nonatomic) IBOutlet UIImageView *vogueStoreImage;
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
 @end
 
@@ -26,6 +27,16 @@
 - (void)setupLoginPageAppearance {
     self.backgroundImage.image = [UIImage imageNamed:@"Bubble_Background"];
     self.vogueStoreImage.image = [UIImage imageNamed:@"Vogue_Store"];
+    
+    [[self.loginButton layer] setBorderWidth:1.0f];
+    [[self.loginButton layer] setCornerRadius:0.0];
+    
+    [self.loginButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.loginButton setBackgroundColor:[UIColor colorWithRed:89 / 255.0 green:116 / 255.0 blue:179 / 255.0 alpha:1.0]];
+    [[self.loginButton layer] setBorderColor:[UIColor whiteColor].CGColor];
+
+    [self.loginButton setTitle:LOGIN_BUTTON_TITLE forState:UIControlStateNormal];
+    
 }
 
 - (void)didReceiveMemoryWarning {
